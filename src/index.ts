e9b5300f0c178c4bdeb8bf6157881be61cd6f84c
Keyur Bhogayata <bhogayata.keyur@gmail.com>
2026-05-05 12:59:08 +0530

    feat(core): export anti-pattern engine from index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 // src/index.ts
 // This file can be used to export core library functionality if needed in the future.
+
+export * from './parser';
+export * from './calculator';
+export * from './advisor';
+export * from './git-reader';
+export * from './antiPatterns';
